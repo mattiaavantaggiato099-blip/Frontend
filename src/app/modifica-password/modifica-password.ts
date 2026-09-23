205f06cc249d2a47096ae5f5ee515d618eda8368
viniciusagostinho05 <viniciusagostinho05@gmail.com>
2026-09-23 09:53:21 +0200

    Add files via upload

---
+++ src/app/modifica-password/modifica-password.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-modifica-password',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './modifica-password.html',
+  styleUrl: './modifica-password.css'
+})
+export class ModificaPasswordComponent {
+
+}
---
+++ src/app/ricarica/ricarica.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-ricarica',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './ricarica.html',
+  styleUrl: './ricarica.css'
+})
+export class RicaricaComponent {
+
+}
